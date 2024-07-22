f55d184d16e3fe401e1c6e7a64714d501ac85cc8
mafortthiago <thmafort@gmail.com>
2024-07-22 09:53:22 -0300

    :tada: feat: Add initial structure

---
+++ src/test/java/com/mafort/rightgrade/RightgradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mafort.rightgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RightgradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
