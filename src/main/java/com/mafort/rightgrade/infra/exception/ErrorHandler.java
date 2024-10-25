5ae96ed9dbb2d1f924793b8289799da06f12ba24
mafortthiago <thmafort@gmail.com>
2024-10-25 15:28:37 -0300

    :bug: fix: Fix handle403error description and unnecessary import

--- src/main/java/com/mafort/rightgrade/infra/exception/ErrorHandler.java
+++ src/main/java/com/mafort/rightgrade/infra/exception/ErrorHandler.java
@@ -1,20 +1,17 @@
 package com.mafort.rightgrade.infra.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @ControllerAdvice
 public class ErrorHandler {
     /**
      * Exception handler that returns an error message and a status code
-     * when the login credentials are incorrect.
+     * when the login are incorrect.
      *
-     * @return ResponseEntity containing the error message and HTTP status code 403 (FORBIDDEN).
+     * @return ResponseEntity containing the error message and HTTP status code 401 (Unauthorized).
      */
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<String> handle403error() {
