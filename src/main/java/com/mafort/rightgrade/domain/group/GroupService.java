971f50defab9add459b8d1c24b475cd0028392d0
mafortthiago <thmafort@gmail.com>
2024-12-11 13:41:06 -0300

    :sparkles: feat: Add a method that gets all groups by ID

--- src/main/java/com/mafort/rightgrade/domain/group/GroupService.java
+++ src/main/java/com/mafort/rightgrade/domain/group/GroupService.java
@@ -1,13 +1,21 @@
 package com.mafort.rightgrade.domain.group;
 
+import com.mafort.rightgrade.domain.page.CustomPage;
 import com.mafort.rightgrade.domain.teacher.Teacher;
 import com.mafort.rightgrade.domain.teacher.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class GroupService {
 
     private final TeacherRepository teacherRepository;
+    @Autowired
+    private GroupRepository groupRepository;
 
     public GroupService(TeacherRepository teacherRepository) {
         this.teacherRepository = teacherRepository;
@@ -18,5 +26,10 @@ public class GroupService {
                 .orElseThrow(() -> new RuntimeException("Teacher not found"));
 
         return new Group(createGroupDTO, teacher);
+    }
+    public CustomPage<GroupListResponseDTO> findAllById(UUID id, Pageable pageable){
+        Page<Group> pages = groupRepository.findByTeacherId(id, pageable);
+        CustomPage<Group> customPages = new CustomPage<>(pages);
+        return customPages.map(p ->new GroupListResponseDTO(p.getId(), p.getName(), p.getMinimumGrade(), p.isGradeFrom0To100()));
     }
 }
