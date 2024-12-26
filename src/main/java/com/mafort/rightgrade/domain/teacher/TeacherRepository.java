bb4bf4ec07c693bc7365328e7ad44af5888eb08c
mafortthiago <thmafort@gmail.com>
2024-12-26 14:38:10 -0300

    feat(Teacher): add find theacher by id method

--- src/main/java/com/mafort/rightgrade/domain/teacher/TeacherRepository.java
+++ src/main/java/com/mafort/rightgrade/domain/teacher/TeacherRepository.java
@@ -3,6 +3,11 @@ package com.mafort.rightgrade.domain.teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
     UserDetails findByEmail(String email);
+
+    Optional<Teacher> findById(UUID uuid);
 }
