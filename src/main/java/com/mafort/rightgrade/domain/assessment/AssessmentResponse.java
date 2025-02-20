f4fa4f0306e545cfdcb6948ae29ca5fb4680eff4
mafortthiago <thmafort@gmail.com>
2025-02-20 20:23:25 -0300

    feat(assessment): modify assessment response DTO

--- src/main/java/com/mafort/rightgrade/domain/assessment/AssessmentResponse.java
+++ src/main/java/com/mafort/rightgrade/domain/assessment/AssessmentResponse.java
@@ -1,9 +1,25 @@
 package com.mafort.rightgrade.domain.assessment;
 
+import com.mafort.rightgrade.domain.grade.Grade;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
-public record AssessmentResponse(UUID id, String name, UUID gradingPeriodId) {
+public record AssessmentResponse(
+        UUID id,
+        String name,
+        UUID gradingPeriodId,
+        LocalDateTime createdAt,
+        double value,
+        List<Grade> grades)
+{
     public AssessmentResponse(Assessment assessment){
-        this(assessment.getId(), assessment.getName(), assessment.getGradingPeriod().getId());
+        this(
+                assessment.getId(),
+                assessment.getName(),
+                assessment.getGradingPeriod().getId(),
+                assessment.getCreatedAt(),
+                assessment.getValue(),
+                assessment.getGrades());
     }
 }
