db5ada5f40b158ac3722eea4bd8350ab88c26f86
mafortthiago <thmafort@gmail.com>
2025-03-09 18:32:01 -0300

    feat((grade): implement grade enitity

---
+++ src/main/java/com/mafort/rightgrade/domain/grade/Grade.java
@@ -0,0 +1,35 @@
+package com.mafort.rightgrade.domain.grade;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mafort.rightgrade.domain.assessment.Assessment;
+import com.mafort.rightgrade.domain.student.Student;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+@NoArgsConstructor
+@Getter
+@Entity
+public class Grade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    @JsonIgnore
+    private Student student;
+    @ManyToOne
+    @JoinColumn(name = "assessment_id")
+    @JsonIgnore
+    private Assessment assessment;
+    @Setter
+    private double value;
+
+    public Grade(CreateGrade createGrade, Assessment assessment, Student student){
+        this.assessment = assessment;
+        this.student = student;
+        this.value = createGrade.value();
+    }
+}
