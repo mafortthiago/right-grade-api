0f1d207b4d32cc8d21ef217c8b640fca94e6030f
mafortthiago <thmafort@gmail.com>
2025-03-24 15:29:29 -0300

    feat(assessment): check added assessments sum

--- src/main/java/com/mafort/rightgrade/domain/assessment/AssessmentService.java
+++ src/main/java/com/mafort/rightgrade/domain/assessment/AssessmentService.java
@@ -3,8 +3,11 @@ package com.mafort.rightgrade.domain.assessment;
 import com.mafort.rightgrade.domain.gradingPeriod.GradingPeriod;
 import com.mafort.rightgrade.domain.gradingPeriod.GradingPeriodRepository;
 import com.mafort.rightgrade.domain.student.Student;
+import com.mafort.rightgrade.infra.exception.InvalidArgumentException;
 import com.mafort.rightgrade.infra.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,9 +20,12 @@ public class AssessmentService {
     private GradingPeriodRepository gradingPeriodRepository;
     @Autowired
     private AssessmentRepository assessmentRepository;
+    @Autowired
+    private MessageSource messageSource;
 
     public Assessment create(CreateAssessment createAssessment){
         GradingPeriod gradingPeriod = findGradingPeriod(createAssessment.gradingPeriodId());
+        this.validateAssessmentSum(createAssessment.value(), createAssessment.gradingPeriodId());
         Assessment assessment = new Assessment(createAssessment, gradingPeriod);
         this.assessmentRepository.save(assessment);
         return assessment;
@@ -41,5 +47,23 @@ public class AssessmentService {
 
     private List<AssessmentResponse> convertToAssessmentResponse(List<Assessment> assessments){
         return assessments.stream().map(a -> new AssessmentResponse(a)).toList();
+    }
+
+    private void validateAssessmentSum(double value, UUID gradingPeriodId){
+        List<Assessment> assessments = this.assessmentRepository.findByGradingPeriodId(gradingPeriodId);
+
+        double sum = 0;
+        if(!assessments.isEmpty()){
+          sum = assessments.stream().mapToDouble(Assessment::getValue).sum();
+        }
+
+        boolean isFrom0To100 = assessments.get(0).getGradingPeriod().getGroup().isGradeFrom0To100();
+        double maxValue = isFrom0To100 ? 100 : 10;
+        double currentValue = value + sum;
+        
+        if(currentValue > maxValue){
+            String errorMessage = messageSource.getMessage("error.invalidAssessmentValue", null, LocaleContextHolder.getLocale());
+            throw new InvalidArgumentException(errorMessage);
+        }
     }
 }
--- src/main/java/com/mafort/rightgrade/domain/assessment/CreateAssessment.java
+++ src/main/java/com/mafort/rightgrade/domain/assessment/CreateAssessment.java
@@ -1,9 +1,6 @@
 package com.mafort.rightgrade.domain.assessment;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -13,6 +10,7 @@ public record CreateAssessment(
         @Size(min = 2, max = 20, message = "Assessment name must be between 2 and 20 characters")
         String name,
         @NotNull(message = "Assessment value is mandatory")
+        @Min(value = 1, message = "{error.minimumAssessmentValue}")
         double value,
         @NotNull(message = "Grading period ID is mandatory")
         UUID gradingPeriodId) {
