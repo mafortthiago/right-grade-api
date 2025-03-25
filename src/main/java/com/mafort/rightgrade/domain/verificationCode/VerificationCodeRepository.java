5ee6ab8d6a7dda02c6b6ffeeab61d7890240210c
mafortthiago <thmafort@gmail.com>
2025-03-25 13:52:37 -0300

    feat(verificationCode): add repository

---
+++ src/main/java/com/mafort/rightgrade/domain/verificationCode/VerificationCodeRepository.java
@@ -0,0 +1,12 @@
+package com.mafort.rightgrade.domain.verificationCode;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface VerificationCodeRepository extends JpaRepository<VerificationCode, UUID> {
+    Optional<VerificationCode> findByEmailAndCode(String email, String code);
+}
